902ea24f6c107030ae8eb1bd5adc89f614971132
gerzin <zinno.gerardo.95@gmail.com>
2024-12-08 23:10:21 +0100

    Add protobuf

--- backend/catalog/cmd/catalog/main.go
+++ backend/catalog/cmd/catalog/main.go
@@ -5,7 +5,22 @@ import (
 	"os"
 )
 
+var (
+	listenAddr = "0:0:0:0"
+	listenPort = "8080"
+)
+
+func readEnv() {
+	if addr := os.Getenv("LISTEN_ADDR"); addr != "" {
+		listenAddr = addr
+	}
+	if port := os.Getenv("LISTEN_PORT"); port != "" {
+		listenPort = port
+	}
+}
+
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	logger.Info("Starting catalog service")
+	readEnv()
 }
