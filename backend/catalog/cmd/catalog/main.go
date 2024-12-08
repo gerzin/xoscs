9ae3d50942bdb61c96ba3fa38db87fa7a7c87a75
gerzin <zinno.gerardo.95@gmail.com>
2024-12-09 00:41:53 +0100

    add mod and pgx

--- backend/catalog/cmd/catalog/main.go
+++ backend/catalog/cmd/catalog/main.go
@@ -10,6 +10,10 @@ var (
 	listenPort = "8080"
 )
 
+// readEnv reads the environment variables LISTEN_ADDR and LISTEN_PORT.
+// If these environment variables are set, it updates the listenAddr and listenPort
+// variables with their respective values. If the environment variables are not set,
+// the default values for listenAddr ("0:0:0:0") and listenPort ("8080") are used.
 func readEnv() {
 	if addr := os.Getenv("LISTEN_ADDR"); addr != "" {
 		listenAddr = addr
--- backend/catalog/internal/db/db.go
+++ backend/catalog/internal/db/db.go
@@ -1,1 +1,16 @@
 package db
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func Connect(uri string) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), uri)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+
+}
