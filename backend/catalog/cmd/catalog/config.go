63dd81d9efe0f076a94132ebca84d7609f1effc5
gerzin <zinno.gerardo.95@gmail.com>
2024-12-09 01:54:48 +0100

    newline

--- backend/catalog/cmd/catalog/config.go
+++ backend/catalog/cmd/catalog/config.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // Configuration settings for the catalog service.
 type Config struct {
 	// Address on which the service will listen.
@@ -8,6 +13,28 @@ type Config struct {
 	// Port on which the service will listen.
 	ListenPort string
 
-	// WURI for connecting to the database.
+	// URI for connecting to the database.
 	DBUri string
+}
+
+func readEnv() (*Config, error) {
+	config := &Config{
+		ListenAddr: "0:0:0:0",
+		ListenPort: "8080",
+	}
+
+	if addr := os.Getenv("LISTEN_ADDR"); addr != "" {
+		config.ListenAddr = addr
+	}
+	if port := os.Getenv("LISTEN_PORT"); port != "" {
+		config.ListenPort = port
+	}
+
+	if db := os.Getenv("DB_URI"); db != "" {
+		config.DBUri = db
+	} else {
+		return nil, fmt.Errorf("DB_URI environment variable not set")
+	}
+
+	return config, nil
 }
--- backend/catalog/cmd/catalog/main.go
+++ backend/catalog/cmd/catalog/main.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log/slog"
 	"os"
 )
-
-func readEnv() (*Config, error) {
-	config := &Config{
-		ListenAddr: "0:0:0:0",
-		ListenPort: "8080",
-	}
-
-	if addr := os.Getenv("LISTEN_ADDR"); addr != "" {
-		config.ListenAddr = addr
-	}
-	if port := os.Getenv("LISTEN_PORT"); port != "" {
-		config.ListenPort = port
-	}
-
-	if db := os.Getenv("DB_URI"); db != "" {
-		config.DBUri = db
-	} else {
-		return nil, fmt.Errorf("DB_URI environment variable not set")
-	}
-
-	return config, nil
-}
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
